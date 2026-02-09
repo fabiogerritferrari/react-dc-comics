e7ffef514f6c6935ade71d0d03655c026c8e50de
fabiogerritferrari <fabiogerritferrari@gmail.com>
2026-02-09 12:51:54 +0100

    creato header con logo dc e componente navbar esterno, con all'interno un array di links inseriti all'interno della nav con map, esportato poi il componente navbar creato e inserito all'interno dell'header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
+import Navbar from "./Navbar"
+
 export default function Header() {
     return <>
         <header>
             <figure>
                 <img src="/src/assets/img/dc-logo.png" alt="" />
             </figure>
+            <Navbar />
         </header>
     </>
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,71 @@
+export default function Navbar() {
+    const Links = [
+        {
+            id: 0,
+            text: 'CHARACTERS',
+            current: false
+        },
+        {
+            id: 1,
+            text: 'COMICS',
+            current: true
+        },
+        {
+            id: 2,
+            text: 'MOVIES',
+            current: false
+        },
+        {
+            id: 3,
+            text: 'TV',
+            current: false
+        },
+        {
+            id: 4,
+            text: 'GAMES',
+            current: false
+        },
+        {
+            id: 5,
+            text: 'COLLECTIBLES',
+            current: false
+        },
+        {
+            id: 6,
+            text: 'VIDEOS',
+            current: false
+        },
+        {
+            id: 7,
+            text: 'FANS',
+            current: false
+        },
+        {
+            id: 8,
+            text: 'NEWS',
+            current: false
+        },
+        {
+            id: 9,
+            text: 'SHOP',
+            current: false
+        }
+    ];
+
+
+    return (
+        <nav id="top-navbar">
+            <ul>
+                {Links.map(link => {
+                    return (
+                        <li key={link.id}>
+                            <a href="#">
+                                {link.text}
+                            </a>
+                        </li>
+                    )
+                })}
+            </ul>
+        </nav>
+    )
+}
