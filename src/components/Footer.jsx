df47b28dd11476e638ad59ff5d27fe76c5390a2e
fabiogerritferrari <fabiogerritferrari@gmail.com>
2026-02-10 12:00:47 +0100

    completato footer aggiungendo un nuovo componente per la navbar inferiore e migliorata la visualizzazione in css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,11 @@
 import FooterNavbar from './FooterNavbar'
+import FooterBottomNavbar from './FooterBottomNavbar'
 
 export default function Footer() {
     return <>
         <footer>
             <FooterNavbar />
+            <FooterBottomNavbar />
         </footer>
     </>
 }
---
+++ src/components/FooterBottomNavbar.jsx
@@ -0,0 +1,20 @@
+export default function FooterBottomNavbar() {
+    return (
+        <div className="bg-grey">
+            <nav className="d-flex container" id="footer-bottom-navbar">
+                <section>
+                    <button id="footer-btn">SIGN-UP NOW</button>
+                </section>
+                <section className="d-flex" id="footer-social">
+                    <h4>FOLLOW US</h4>
+                    <i class="bi bi-facebook"></i>
+                    <i class="bi bi-twitter-x"></i>
+                    <i class="bi bi-youtube"></i>
+                    <i class="bi bi-pinterest"></i>
+                    <i class="bi bi-geo-alt"></i>
+                </section>
+            </nav>
+        </div>
+    )
+
+}
